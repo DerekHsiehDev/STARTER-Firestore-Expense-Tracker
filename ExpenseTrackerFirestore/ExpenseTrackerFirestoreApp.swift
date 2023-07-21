173eb44e14498360ecb1742a7589ed2c473a6aa4
Derek Hsieh <devderekhsieh@gmail.com>
2023-07-20 18:41:46 -0700

    Initial Commit

---
+++ ExpenseTrackerFirestore/ExpenseTrackerFirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerFirestoreApp.swift
+//  ExpenseTrackerFirestore
+//
+//  Created by Derek Hsieh on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerFirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
